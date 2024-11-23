4864595fd2b9e67c9c945651f40791733a86b1e0
Lucas Seccatto <lucasseccatto1@gmail.com>
2024-11-23 16:06:03 -0300

    Integra Prisma e implementa CRUD completo para usuarios

--- server.js
+++ server.js
@@ -1,18 +1,53 @@
 import express from "express";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
 
 const app = express();
 app.use(express.json());
 
-const users = [];
+app.get("/usuarios", async (req, res) => {
+  const users = await prisma.user.findMany();
 
-app.get("/usuarios", (req, res) => {
   res.status(200).json(users);
 });
 
-app.post("/usuarios", (req, res) => {
-  users.push(req.body);
+app.post("/usuarios", async (req, res) => {
+  const user = await prisma.user.create({
+    data: {
+      email: req.body.email,
+      age: req.body.age,
+      name: req.body.name,
+    },
+  });
 
-  res.status(201).json({ message: "Usuário criado com sucesso!" });
+  res.status(201).json(user);
+});
+
+app.put("/usuarios/:id", async (req, res) => {
+  const user = await prisma.user.update({
+    where: {
+      id: req.params.id,
+    },
+
+    data: {
+      email: req.body.email,
+      age: req.body.age,
+      name: req.body.name,
+    },
+  });
+
+  res.status(201).json(user);
+});
+
+app.delete("/usuarios/:id", async (req, res) => {
+  await prisma.user.delete({
+    where: {
+      id: req.params.id,
+    },
+  });
+
+  res.status(200).json({ message: "Usuário deletado com sucesso!" });
 });
 
 app.listen(3000);
